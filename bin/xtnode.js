60c31f613faa18b7953471f5fb8ecbc3c86174ca
DAC <dac@cogsmith.com>
2021-05-09 09:32:50 -0400

    .

--- bin/xtnode.js
+++ bin/xtnode.js
@@ -7,16 +7,16 @@ let nodeapp = args.shift();
 
 //console.log({NODEBIN:nodebin,NODEAPP:nodeapp,ARGS:args});
 
-if (false) {}
-else if (args[0]=='version' || args[1]=='version') {
+if (false) { }
+else if (args[0] == 'version' || args[1] == 'version') {
     console.log(XT.Meta.Version);
 }
-else if (args[0]=='evalxtnode' || args[1]=='evalxtnode') {
+else if (args[0] == 'evalxtnode' || args[1] == 'evalxtnode') {
     let evalxtnode = fs.readFileSync('/bin/evalxtnode').toString();
     console.log(evalxtnode);
 }
 else if (fs.existsSync('/app/app.js')) {
-    let cmd = 'node /app/app.js '+args.join(' ');
+    let cmd = 'node /app/app.js ' + args.join(' ');
     XT.EXECA.command(cmd).stdout.pipe(process.stdout);
 }
 else {
